365d1ec120380abade9b69a089d9930eadb6c301
shender11 <shender1999@gmail.com>
2026-05-05 16:22:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import asyncio
 
-from bots.breakfamily import start_bot
+from bots.breakfamily import start_bot as start_breakfamily
+from bots.radisson import start_bot as start_radisson
+from bots.sharkteambreaks import start_bot as start_sharkteambreaks
+from bots.cometeambreaks import start_bot as start_cometeambreaks
+from bots.lariusno import start_bot as start_lariusno
+from bots.delta import start_bot as start_delta
 
 
 async def main():
-    await start_bot()
+    await asyncio.gather(
+        start_breakfamily(),
+        start_radisson(),
+        start_sharkteambreaks(),
+        start_cometeambreaks(),
+        start_lariusno(),
+        start_delta(),
+    )
 
 
 if __name__ == "__main__":
