607bb66fabac332438c69dfa24276c9a1e8bce7a
shender11 <shender1999@gmail.com>
2026-05-05 12:43:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("all-bots started")
+import asyncio
+
+from bots.breakfamily import start_bot
+
+
+async def main():
+    await start_bot()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
