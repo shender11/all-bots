2d361113652f96bc513acae82ffdd32be2b5065a
shender11 <shender1999@gmail.com>
2026-05-22 14:19:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
 
-from bots.breakfamily import start_bot as start_breakfamily
 from bots.radisson import start_bot as start_radisson
 from bots.sharkteambreaks import start_bot as start_sharkteambreaks
 from bots.cometeambreaks import start_bot as start_cometeambreaks
 from bots.lariusno import start_bot as start_lariusno
 from bots.delta import start_bot as start_delta
+from bots.Energy import start_bot as start_energy
 
 
 async def delayed_start(delay, starter):
@@ -15,12 +15,12 @@ async def delayed_start(delay, starter):
 
 async def main():
     await asyncio.gather(
-        delayed_start(0, start_breakfamily),
         delayed_start(5, start_radisson),
         delayed_start(10, start_sharkteambreaks),
         delayed_start(15, start_cometeambreaks),
         delayed_start(20, start_lariusno),
         delayed_start(25, start_delta),
+        delayed_start(30, start_energy),
     )
 
 
